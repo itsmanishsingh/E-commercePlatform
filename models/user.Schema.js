99a584f80f86d2fe81270a55e231dd81db4066d6
Manish singh <85191003+itsmanishsingh@users.noreply.github.com>
2023-01-15 19:39:40 +0000

    code update

--- models/user.Schema.js
+++ models/user.Schema.js
@@ -1,6 +1,9 @@
 import mongoose from "mongoose";
 
-
+/*
+ const userSchema = new mongoose.Schema({})
+The above and below code are the same ie the "new" word is optional
+*/
 const userSchema = mongoose.Schema(
     {
         name:{
