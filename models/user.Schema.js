c73dbf3f54621e9f4b56f10c24a85897049977bf
Manish singh <85191003+itsmanishsingh@users.noreply.github.com>
2023-01-15 20:43:06 +0000

    added UserSchema

--- models/user.Schema.js
+++ models/user.Schema.js
@@ -1,16 +1,41 @@
-import mongoose from "mongoose";
-
+import mongoose, { model } from "mongoose";
+import AuthRoles from '../utils/authRoles';
 /*
  const userSchema = new mongoose.Schema({})
 The above and below code are the same ie the "new" word is optional
 */
 const userSchema = mongoose.Schema(
     {
-        name:{
-            type:String,
-            required :[true , "Name is required"],
-            maxLength :[50 , "Name must be less than 50"]
-        }
+        name: {
+            type: String,
+            required: [true, "Name is required"],
+            maxLength: [50, "Name must be less than 50"]
+        },
+
+        email: {
+            type: String,
+            required: [true, "Email is required"],
+            unique: true
+        },
+
+        password: {
+            type: String,
+            required: [true, "Password is required"],
+            minLength: [8, "Password must be atleast of 8 characters"],
+            select: false
+        },
+
+        role:{
+            type : String,
+            enum : Object.values(AuthRoles),
+            default : AuthRoles.USER
+        },
+        forgotPasswordToken: String ,
+        forgotPasswordExpiry: Date ,
+    },
+    {
+        timestamps : true
     }
-    
-    )
+);
+
+module.exports = mongoose.model("User",userSchema)
