89e87f26ce056426c02617edd117603937a643e6
itsmanishsingh <itsmanishsingh6@gmail.com>
2023-01-31 00:18:58 +0530

    Server Files removed

--- config/index.js
+++ config/index.js
@@ -5,6 +5,7 @@ dotenv.config();
 const config = {
     JWT_SECRETKEY : process.env.JWT_SECRETKEY,
     JWT_EXPIRY : process.env.JWT_EXPIRY || '30d',
+    MONGODB_URL:process.env.MONGODB_URL
 }
 
 export default config;
