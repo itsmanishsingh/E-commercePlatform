4857e24ae5a422c1e0f3244cefbb3bf787453701
itsmanishsingh <itsmanishsingh6@gmail.com>
2023-01-30 19:07:42 +0530

    Order Schema Updated

---
+++ models/order.Schema.js
@@ -0,0 +1,52 @@
+import mongoose from "mongoose";
+
+const orderSchema = new mongoose.Schema(
+    {
+        product:{
+            type:[
+                {
+                    productId:{
+                        type:mongoose.Schema.Types.ObjectId,
+                        ref:"Product",
+                        required:true
+                    },
+                    count:Number,
+                    price:Number
+                }
+            ],
+            required:true
+        },
+        user:{
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"User",
+            required:true
+        },
+        address:{
+            type:String,
+            required:true
+        },
+        phoneNumber:{
+            type:Number,
+            required:true,
+        },
+        amount:{
+            type:Number,
+            required:true
+        },
+        coupon: String,
+        transactionId:Number,
+        status:{
+            type:String,
+            enum: ["ORDERED", "SHIPPED", "DELIVERED", "CANCELLED"],
+            default:"ORDERED"
+            // enum: Object.values.(DeliveryStatus)
+            // default : DeliveryStatus.ORDERED
+        }
+
+    },
+    {
+        timestamps:true
+    }
+)
+
+export default mongoose.model('Order',orderSchema);
