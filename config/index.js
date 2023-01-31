235a5408fa2d7994ecee51409390cf92c9aadb33
itsmanishsingh <itsmanishsingh6@gmail.com>
2023-01-31 15:05:48 +0530

    Updated the PORT Number

--- config/index.js
+++ config/index.js
@@ -5,7 +5,8 @@ dotenv.config();
 const config = {
     JWT_SECRETKEY : process.env.JWT_SECRETKEY,
     JWT_EXPIRY : process.env.JWT_EXPIRY || '30d',
-    MONGODB_URL:process.env.MONGODB_URL
+    MONGODB_URL:process.env.MONGODB_URL,
+    PORT :process.env.PORT
 }
 
 export default config;
