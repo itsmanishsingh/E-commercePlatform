5fbc2ad664e8fc55c245af63e94a950ad4eb9638
itsmanishsingh <itsmanishsingh6@gmail.com>
2023-01-31 15:03:25 +0530

    Connected to DB using IIFE

---
+++ index.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+import app from "./app";
+import config from "./config/index";
+
+
+//create a function-> Run the funciton
+// "()() :- IIFE (Immediately Invoked Function Expression) or This type is used when the function is defined and executed immediately
+//   (async ()=>{})()
+( async () => {
+    try {
+        await mongoose.connect(config.MONGODB_URL)
+        console.log(`Connected to Database`);
+        
+        // app.on('mount', callback(parent))  :- The mount event is fired on a sub-app, when it is mounted on a parent app. The parent app is passed to the callback function.
+        app.on('error', (err) => {
+            console.log("ERROR: ", err);
+            throw err;
+        })
+
+        const onlistening = () => {
+            console.log(`Application is running at ${config.PORT}`);
+        }
+
+        app.listen(config.PORT , onlistening)
+
+    } catch (err) {
+        console.log("ERROR", err);
+        throw err;
+    }
+
+})()
